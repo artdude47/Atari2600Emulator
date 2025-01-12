57c2e3ec386d3de51c8a69d1acfa048f4828978f
artdude47 <artcouser@gmail.com>
2025-01-11 19:22:28 -0500

    Added basic memory and cpu class

---
+++ Atari2600Emulator/Core/AtariMemory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atari2600Emulator.Core
+{
+    internal class AtariMemory
+    {
+        private byte[] _memory;
+
+        public AtariMemory(int size = 0x10000) // 64KB
+        {
+            _memory = new byte[size];
+        }
+
+        public byte ReadByte(ushort address)
+        {
+            return _memory[address];
+        }
+
+        public void WriteByte(ushort address, byte value)
+        {
+            _memory[address] = value; 
+        }
+    }
+}
---
+++ Atari2600Emulator/Core/CPU6507.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atari2600Emulator.Core
+{
+    internal class CPU6507
+    {
+        private AtariMemory _memory;
+
+        //Registers
+        private byte A, X, Y;   //Accumulator, Index Registers
+        private ushort PC;      //Program counter
+        private byte SP;        //Stack Pointer
+        private byte P;         //Processor Status
+
+        public CPU6507(AtariMemory memory)
+        {
+            _memory = memory;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            PC = (ushort)(_memory.ReadByte(0xFFFC) | (_memory.ReadByte(0xFFFD) << 8));
+            SP = 0xFF;
+            P = 0x34;
+        }
+
+        public void Step()
+        {
+            byte opcode = _memory.ReadByte(PC);
+            PC++;
+
+            DecodeAndExecuteOpcode(opcode);
+        }
+
+        private void DecodeAndExecuteOpcode(byte opcode)
+        {
+            switch (opcode)
+            {
+                case 0xEA: //NOP
+                    break;
+                default:
+                    throw new NotImplementedException($"Opcode {opcode:X2} not implemented");
+            }
+    }
+}
